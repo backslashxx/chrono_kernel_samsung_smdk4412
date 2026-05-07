41bc566bebeebac43dd9b2312d438da7d03f6f5c
Davidlohr Bueso <dave@stgolabs.net>
2026-05-07 04:29:13 -0700

    BACKPORT: locking/rtmutex: Skip remove_waiter() when waiter is not enqueued
    (cherry picked from commit 3bfdc63936dd4773109b7b8c280c0f3b5ae7d349)
    
    syzbot triggered the following splat in remove_waiter() via
    FUTEX_CMP_REQUEUE_PI:
    
      KASAN: null-ptr-deref in range [0x0000000000000a88-0x0000000000000a8f]
       class_raw_spinlock_constructor
       remove_waiter+0x159/0x1200 kernel/locking/rtmutex.c:1561
       rt_mutex_start_proxy_lock+0x103/0x120
       futex_requeue+0x10e4/0x20d0
       __x64_sys_futex+0x34f/0x4d0
    
    task_blocks_on_rt_mutex() does not arm the waiter upon deadlock detection,
    leaving waiter->task nil, where 3bfdc63936dd ("rtmutex: Use waiter::task instead
    of current in remove_waiter()") made this fatal.
    
    Furthermore, rt_mutex_start_proxy_lock() should not be calling into remove_waiter()
    upon a successfully grabbing the rtmutex. 1a1fb985f2e2 ("futex: Handle early deadlock
    return correctly"), moved the remove_waiter() out of __rt_mutex_start_proxy_lock()
    (where 'ret' was only ever 0 or < 0) into the wrapper. Tighten this check to
    account for try_to_take_rt_mutex().
    
    Fixes: 3bfdc63936dd ("rtmutex: Use waiter::task instead of current in remove_waiter()")
    Reported-by: syzbot+78147abe6c524f183ee9@syzkaller.appspotmail.com
    Signed-off-by: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    Closes: https://lore.kernel.org/all/69f114ac.050a0220.ac8b.0003.GAE@google.com/
    Link: https://patch.msgid.link/20260507112913.1019537-1-dave@stgolabs.net

--- kernel/rtmutex.c
+++ kernel/rtmutex.c
@@ -498,6 +498,9 @@ static void remove_waiter(struct rt_mutex *lock,
 	unsigned long flags;
 	int chain_walk = 0;
 
+	if (!waiter_task) /* never enqueued */
+		return;
+
 	raw_spin_lock_irqsave(&waiter_task->pi_lock, flags);
 	plist_del(&waiter->list_entry, &lock->wait_list);
 	waiter_task->pi_blocked_on = NULL;
@@ -973,7 +976,7 @@ int rt_mutex_start_proxy_lock(struct rt_mutex *lock,
 		ret = 0;
 	}
 
-	if (unlikely(ret))
+	if (unlikely(ret < 0))
 		remove_waiter(lock, waiter);
 
 	raw_spin_unlock(&lock->wait_lock);
