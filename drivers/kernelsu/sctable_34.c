8776c6258a50abac5fbb97dffc67fbb618260c7c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-09 15:08:15 +0800

    1

--- drivers/kernelsu/sctable_34.c
+++ drivers/kernelsu/sctable_34.c
@@ -90,13 +90,13 @@ static void read_and_replace_syscall(void *old_ptr, unsigned long syscall_nr, vo
 
 	*(void **)old_ptr = FORCE_VOLATILE(*syscall_addr);
 
-	preempt_disable();
-	local_irq_disable();
+//	preempt_disable();
+//	local_irq_disable();
 
 	FORCE_VOLATILE(*syscall_addr) = new_ptr;
 
-	local_irq_enable();
-	preempt_enable();
+//	local_irq_enable();
+//	preempt_enable();
 
 	flush_cache_all(); // this is important!
 	smp_mb();
@@ -104,20 +104,24 @@ static void read_and_replace_syscall(void *old_ptr, unsigned long syscall_nr, vo
 	return;
 }
 
-
-
-static __init int ksu_syscall_table_hook_init()
+static int patch_sctable_stop_machine()
 {
 	unsigned long *sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
 
 	read_and_replace_syscall((void *)&armeabi_reboot, __ARMEABI_reboot, (void *)hook_armeabi_reboot, (void *)sys_call_table);
 
-	read_and_replace_syscall((void *)&armeabi_execve, __ARMEABI_execve, (void *)hook_armeabi_execve, (void *)sys_call_table);
-	read_and_replace_syscall((void *)&armeabi_faccessat, __ARMEABI_faccessat, (void *)hook_armeabi_faccessat, (void *)sys_call_table);
-	read_and_replace_syscall((void *)&armeabi_fstatat64, __ARMEABI_fstatat64, (void *)hook_armeabi_fstatat64, (void *)sys_call_table);
+//	read_and_replace_syscall((void *)&armeabi_execve, __ARMEABI_execve, (void *)hook_armeabi_execve, (void *)sys_call_table);
+//	read_and_replace_syscall((void *)&armeabi_faccessat, __ARMEABI_faccessat, (void *)hook_armeabi_faccessat, (void *)sys_call_table);
+//	read_and_replace_syscall((void *)&armeabi_fstatat64, __ARMEABI_fstatat64, (void *)hook_armeabi_fstatat64, (void *)sys_call_table);
 
 	read_and_replace_syscall((void *)&armeabi_fstat64, __ARMEABI_fstat64, (void *)hook_armeabi_fstat64_ret, (void *)sys_call_table);
-	read_and_replace_syscall((void *)&armeabi_read, __ARMEABI_read, (void *)hook_armeabi_read, (void *)sys_call_table);
+//	read_and_replace_syscall((void *)&armeabi_read, __ARMEABI_read, (void *)hook_armeabi_read, (void *)sys_call_table);
+	return 0;
+}
+
+static __init int ksu_syscall_table_hook_init()
+{
+	stop_machine(patch_sctable_stop_machine, NULL, NULL);
 
 	return 0;
 }
